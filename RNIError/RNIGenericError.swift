2e247aa355ec628d8097dcf84f0bea5ba5b5722d
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2022-04-21 17:45:32 +0800

    ⭐️ Implement: Add `RNIGenericError`

---
+++ RNIError/RNIGenericError.swift
@@ -0,0 +1,25 @@
+//
+//  RNIGenericError.swift
+//  react-native-ios-utilities
+//
+//  Created by Dominic Go on 4/21/22.
+//
+
+import Foundation
+
+
+class RNIGenericError: RNIBaseError<RNIGenericErrorCodes> {
+  
+  init(
+    code: RNIGenericErrorCodes,
+    message: String? = nil,
+    debug: String? = nil
+  ) {
+    super.init(
+      code: code,
+      domain: "react-native-ios-utilities",
+      message: message,
+      debug: debug
+    );
+  };
+};
