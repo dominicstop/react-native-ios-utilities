24b9e37ef6cfc859de56a585253ced1434512e92
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2022-08-28 11:11:07 +0800

    🆕 Add: Types - `RNIError`
    Update RNIError.ts

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,6 @@
 export * from './functions/Helpers';
 
+export * from './types/RNIError';
+
 export * from './native_components/RNIWrapperView';
 export * from './native_modules/RNIWrapperViewModule';
