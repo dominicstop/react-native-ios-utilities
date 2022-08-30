7ebb622ddf7bd25c12ed74eb7737a392adeeaa91
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2022-08-30 13:16:16 +0800

    🆕 Add: Error-Handling Related

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,11 @@
 export * from './functions/Helpers';
+export * from './functions/ErrorUtilities';
 export * from './functions/TypeUtils';
 
 export * from './types/RNIError';
+export * from './types/NativeError';
+
+export * from './constants/RNIBaseErrorCodes';
 
 export * from './native_components/RNIWrapperView';
 export * from './native_modules/RNIWrapperViewModule';
