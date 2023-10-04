28897048c2f1c53c7434f9a23f5122ccb4981393
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2023-10-04 21:47:21 +0800

    🆕 Add: `RNIImage`
    Related:
    * `TODO:2023-10-05-12-30-30` Impl: Re-add prev. deleted sources, and update/re-write them.

---
+++ ios/RNIImage/RNIImageOptions.swift
@@ -0,0 +1,42 @@
+//
+//  RNIImageOptions.swift
+//  react-native-ios-context-menu
+//
+//  Created by Dominic Go on 9/28/22.
+//
+
+import Foundation
+
+
+public struct RNIImageOptions {
+
+  // MARK: - Properties
+  // ------------------
+
+  var tint: UIColor?;
+  var renderingMode: UIImage.RenderingMode;
+  var cornerRadius: CGFloat?;
+  
+  // MARK: - Init
+  // ------------
+  
+  init(dict: NSDictionary){
+    self.tint = {
+      guard let value = dict["tint"],
+            let color = UIColorHelpers.parseColor(value: value)
+      else { return nil };
+      
+      return color;
+    }();
+    
+    self.renderingMode = {
+      guard let string = dict["renderingMode"] as? String,
+            let mode = UIImage.RenderingMode(string: string)
+      else { return .automatic };
+      
+      return mode;
+    }();
+    
+    self.cornerRadius = dict["cornerRadius"] as? CGFloat;
+  };
+};
