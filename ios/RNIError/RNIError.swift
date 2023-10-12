3d4a27f4f630d0a401168e475091db6822189064
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2023-10-12 23:18:43 +0800

    🛠 Refactor: `RNIError`

--- ios/RNIError/RNIError.swift
+++ ios/RNIError/RNIError.swift
@@ -7,6 +7,67 @@
 
 import Foundation
 
+public struct RNIError: LocalizedError {
+  
+  public var domain: String;
+  public var description: String;
+  public var extraDebugInfo: String?;
+
+  public var fileName: String;
+  public var lineNumber: Int;
+  public var columnNumber: Int;
+  public var functionName: String;
+  
+  // MARK: - Computed Properties
+  // ---------------------------
+  
+  public var debugTrace: String {
+      "fileName: \(self.fileName)"
+    + " - functionName: \(self.functionName)"
+    + " - lineNumber: \(self.lineNumber)"
+    + " - columnNumber: \(self.columnNumber)"
+  };
+  
+  public var baseErrorMessage: String {
+    var errorMessage =
+        "domain: \(self.domain)"
+      + " - description: \(self.description)";
+  
+    if let extraDebugInfo = self.extraDebugInfo {
+      errorMessage += " - extraDebugInfo: \(extraDebugInfo)";
+    };
+    
+    return errorMessage;
+  };
+  
+  public var errorDescription: String? {
+    self.baseErrorMessage + " - \(self.debugTrace)";
+  };
+  
+  // MARK: - Init
+  // ------------
+  
+  public init(
+    domain: String,
+    description: String,
+    extraDebugInfo: String,
+    fileName: String = #file,
+    lineNumber: Int = #line,
+    columnNumber: Int = #column,
+    functionName: String = #function
+  ) {
+  
+    self.domain = domain;
+    self.description = description;
+    self.extraDebugInfo = extraDebugInfo;
+    
+    self.fileName = fileName;
+    self.lineNumber = lineNumber;
+    self.columnNumber = columnNumber;
+    self.functionName = functionName;
+  };
+};
+
 
 open class RNIBaseError<E: RawRepresentable>: Error where E.RawValue == String  {
   
