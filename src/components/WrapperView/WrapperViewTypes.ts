866d66d01be4991a946b1898439d34079c8eb8c0
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2023-10-18 20:59:56 +0800

    💫 Update: Types - `WrapperViewTypes`

--- src/components/WrapperView/WrapperViewTypes.ts
+++ src/components/WrapperView/WrapperViewTypes.ts
@@ -1,3 +1,12 @@
+import type { LegacyRef } from "react";
 import type { ViewProps } from "react-native";
 
-export type WrapperViewProps = ViewProps;
+import type { WrapperView } from "./WrapperView";
+
+export type WrapperViewBaseProps = {
+  ref: LegacyRef<WrapperView> | undefined;
+};
+
+export type WrapperViewProps = 
+  & WrapperViewBaseProps
+  & Exclude<ViewProps, 'ref'>;
