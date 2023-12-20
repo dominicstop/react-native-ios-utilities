e854c908d90dcd0bab0807e8b50981b3e46383c0
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2023-12-21 06:57:54 +0800

    🆕 Add: Import Sources from `react-native-ios-adaptive-modal`

---
+++ src/types/NativeTypes/UIGeometry.ts
@@ -0,0 +1,13 @@
+
+// TODO: Move to `react-native-ios-utilities`
+export type UIEdgeInsets = {
+  top: number;
+  left: number;
+  bottom: number;
+  right: number;
+};
+
+export type UIOffset = {
+  horizontal?: number;
+  vertical?: number;
+};
--- src/types/UtilityTypes.ts
+++ src/types/UtilityTypes.ts
@@ -3,3 +3,6 @@ export type KeyMapType
   <T extends string, K extends { [k in `${T}`]: any }> = K;
 
 export type FunctionVoid = () => void;
+
+export type KeysWithType<T, U> = 
+  { [K in keyof T]: T[K] extends U ? K : never }[keyof T];
