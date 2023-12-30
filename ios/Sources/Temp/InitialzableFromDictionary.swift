44331d59750b5f5839bf1b2429abbdfc9cccced7
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2023-12-31 05:54:43 +0800

    💫 Update: `InitialzableFromDictionary`

--- ios/Sources/Temp/InitialzableFromDictionary.swift
+++ ios/Sources/Temp/InitialzableFromDictionary.swift
@@ -13,3 +13,13 @@ public protocol InitializableFromDictionary {
   
   init(fromDict dict: Dictionary<String, Any>) throws;
 };
+
+extension InitializableFromDictionary {
+  
+  // For backwards compatibility
+  @available(*, deprecated, message: "Please use init(fromDict:) instead...")
+  init?(dict: Dictionary<String, Any>){
+    guard let value = try? Self.init(fromDict: dict) else { return nil };
+    self = value;
+  };
+};
