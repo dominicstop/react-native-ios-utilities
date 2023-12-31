b77ce1d781c79d8504ec32e3584d06ef132ebeda
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2023-12-31 08:43:58 +0800

    🛠 Refactor: Replace `UIViewAnimationOptions+Init` w/ `UIViewAnimate+InitializableFromString`

---
+++ Extensions+InitializableFromString/UIViewAnimate+InitializableFromString.swift
@@ -0,0 +1,36 @@
+//
+//  UIViewAnimate+InitializableFromString.swift
+//  react-native-ios-context-menu
+//
+//  Created by Dominic Go on 6/18/22.
+//
+
+import UIKit
+
+extension UIView.AnimationOptions: InitializableFromString {
+
+  public init(fromString string: String) throws {
+    switch string {
+      case "curveEaseIn":
+        self = .curveEaseIn;
+        
+      case "curveEaseOut":
+        self = .curveEaseOut;
+        
+      case "curveEaseInOut":
+        self = .curveEaseInOut;
+        
+      case "curveLinear":
+        self = .curveLinear;
+        
+      default:
+        throw RNIUtilitiesError(
+          errorCode: .invalidArgument,
+          description: "Invalid string value",
+          extraDebugValues: [
+            "string": string,
+          ]
+        );
+    };
+  };
+};
