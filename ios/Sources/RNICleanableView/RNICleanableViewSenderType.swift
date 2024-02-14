fffba90f668fc210f6c925b221bda62b5a074efa
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2024-02-15 00:24:43 +0800

    ⭐️ Impl: `RNICleanableViewSenderType.rawSender`

--- ios/Sources/RNICleanableView/RNICleanableViewSenderType.swift
+++ ios/Sources/RNICleanableView/RNICleanableViewSenderType.swift
@@ -8,6 +8,7 @@
 import UIKit
 import ExpoModulesCore
 
+
 public enum RNICleanableViewSenderType {
   case unspecified;
   case unsupported(Any);
@@ -20,4 +21,26 @@ public enum RNICleanableViewSenderType {
     reactTag: Int?,
     commandArguments: Dictionary<String, Any>?
   );
+  
+  // MARK: - Computed Properties
+  // ---------------------------
+  
+  var rawSender: Any? {
+    switch self {
+      case let .unsupported(sender):
+        return sender;
+        
+      case let .view(sender):
+        return sender;
+        
+      case let .viewController(sender):
+        return sender;
+        
+      case let .cleanableViewDelegate(sender):
+        return sender;
+
+      default:
+        return nil;
+    };
+  };
 };
