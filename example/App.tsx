75b8f3da25134d6285cc2e75aac0a765173a8e61
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2024-02-20 00:01:59 +0800

    💫 Update: Ex - App Entry

--- example/App.tsx
+++ example/App.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
 
 import { HomeScreen } from './src/screens/HomeScreen';
+import { setSharedEnvForRNICleanableViewRegistry, setSharedEnvForRNIUtilitiesModule } from 'react-native-ios-utilities';
 
 export default function App() {
   return (
     <HomeScreen/>
-);
-}
+  );
+};
+
+setSharedEnvForRNIUtilitiesModule({
+  debugShouldLogViewRegistryEntryRemoval: true,
+  overrideEnableLogStackTrace: true,
+  overrideShouldLogFileMetadata: true,
+  overrideShouldLogFilePath: true
+});
+
+setSharedEnvForRNICleanableViewRegistry({
+  debugShouldLogCleanup: true,
+  debugShouldLogRegister: true,
+});
