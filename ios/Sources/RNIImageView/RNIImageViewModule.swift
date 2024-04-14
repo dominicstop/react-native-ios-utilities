87ef42633de626aa96fff4715706546fe138a972
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2024-04-14 23:09:20 +0800

    ⭐️ Impl: `RNIImageViewModule.getAllSFSymbols`

--- ios/Sources/RNIImageView/RNIImageViewModule.swift
+++ ios/Sources/RNIImageView/RNIImageViewModule.swift
@@ -14,6 +14,32 @@ public class RNIImageViewModule: Module {
   public func definition() -> ModuleDefinition {
     Name("RNIImageView");
     
+    AsyncFunction("getAllSFSymbols") { (promise: Promise) in
+      #if DEBUG
+      guard let sfSymbolsBundle = Bundle(identifier: "com.apple.SFSymbolsFramework"),
+            let bundlePath = sfSymbolsBundle.path(forResource: "CoreGlyphs", ofType: "bundle"),
+            let bundle = Bundle(path: bundlePath),
+            let resourcePath = bundle.path(forResource: "symbol_search", ofType: "plist"),
+            let plist = NSDictionary(contentsOfFile: resourcePath)
+      else {
+        let error = RNIUtilitiesError(
+          errorCode: .unexpectedNilValue,
+          description: "Unable to get bundle for SFSymbols"
+        );
+        promise.reject(error);
+        return;
+      };
+      
+      promise.resolve(plist);
+      #else
+      let error = RNIUtilitiesError(
+        errorCode: .runtimeError,
+        description: "Not supported in production builds"
+      );
+      promise.reject(error);
+      #endif
+    };
+    
     View(RNIImageView.self) {
       Prop("imageConfig"){
         $0.imageConfigProp = $1;
--- src/native_components/RNIImageView/RNIImageViewModule.ts
+++ src/native_components/RNIImageView/RNIImageViewModule.ts
@@ -1,6 +1,7 @@
 import { requireNativeModule } from 'expo-modules-core';
 
 interface RNIImageViewModule {
+  getAllSFSymbols: () => Promise<Record<string, Array<string>>>;
 };
 
 export const RNIImageViewModule: RNIImageViewModule = 
