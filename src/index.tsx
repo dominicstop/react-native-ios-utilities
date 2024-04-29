b885170a70ab901f55fa32aa0087392c224aa4de
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2024-04-29 13:11:51 +0800

    🛠 Refactor: Move `IosUtilitiesViewNativeComponent`

--- src/index.tsx
+++ src/index.tsx
@@ -1,2 +1,1 @@
-export { default as IosUtilitiesView } from './IosUtilitiesViewNativeComponent';
-export * from './IosUtilitiesViewNativeComponent';
+export * from './native_components/IosUtilitiesView';
