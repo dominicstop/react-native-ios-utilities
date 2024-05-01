5201d3ce49f34276248e63cbb9bb7a31d54e75d5
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2024-05-02 00:30:08 +0800

    🐞 Fix: Forward Declarations in `RNIObjcUtils`

--- ios/Sources/Common/RNIObjcUtils.h
+++ ios/Sources/Common/RNIObjcUtils.h
@@ -5,9 +5,9 @@
 //  Created by Dominic Go on 4/30/24.
 //
 
-
 @class RNILayoutMetrics;
 
+#if __cplusplus
 namespace facebook::react {
   struct LayoutMetrics;
 }
@@ -15,12 +15,13 @@ namespace facebook::react {
 namespace folly {
   struct dynamic;
 }
+#endif
 
 @interface RNIObjcUtils : NSObject
-
+#if __cplusplus
 + (id)convertFollyDynamicToId: (const folly::dynamic*)dyn;
 
 + (RNILayoutMetrics *)createRNILayoutMetricsFrom: (facebook::react::LayoutMetrics)layoutMetrics;
-
+#endif
 @end
 
