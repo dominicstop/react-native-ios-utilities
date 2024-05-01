59fd445aacfac4c26bb28f58d1cfb08727d48da7
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2024-05-01 17:32:39 +0800

    💫 Update: `IosUtilitiesViewNativeComponent`

--- src/native_components/IosUtilitiesView/IosUtilitiesViewNativeComponent.ts
+++ src/native_components/IosUtilitiesView/IosUtilitiesViewNativeComponent.ts
@@ -1,8 +1,10 @@
 import codegenNativeComponent from 'react-native/Libraries/Utilities/codegenNativeComponent';
-import type { ViewProps } from 'react-native';
+import type { HostComponent, ViewProps } from 'react-native';
 
 interface NativeProps extends ViewProps {
   color?: string;
 }
 
-export default codegenNativeComponent<NativeProps>('IosUtilitiesView');
+export default codegenNativeComponent<NativeProps>('IosUtilitiesView', {
+  excludedPlatforms: ['android'],
+}) as HostComponent<NativeProps>;
