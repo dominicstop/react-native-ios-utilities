3e565f98d4381ec147b817b04bc35b164c4dd783
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2024-05-07 16:35:06 +0800

    ⭐️ Impl: `RNIBaseViewComponentDescriptor`

--- ios/Sources/IosUtilitiesView/IosUtilitiesViewComponentDescriptor.h
+++ ios/Sources/IosUtilitiesView/IosUtilitiesViewComponentDescriptor.h
@@ -7,58 +7,18 @@
 #pragma once
 
 #include "IosUtilitiesViewShadowNode.h"
+#include "RNIBaseViewComponentDescriptor.h"
 
-#include <react/debug/react_native_assert.h>
+#include <react-native-ios-utilities/RNIBaseViewState.h>
 #include <react/renderer/core/ConcreteComponentDescriptor.h>
 
-
-namespace facebook {
-namespace react {
+namespace facebook::react {
 
 class IosUtilitiesViewComponentDescriptor final
-  : public ConcreteComponentDescriptor<IosUtilitiesViewShadowNode> {
+  : public RNIBaseViewComponentDescriptor<IosUtilitiesViewShadowNode> {
   
-  public:
-    using ConcreteComponentDescriptor::ConcreteComponentDescriptor;
-
-  void adopt(ShadowNode& shadowNode) const override {
-    react_native_assert(
-      dynamic_cast<IosUtilitiesViewShadowNode*>(&shadowNode)
-    );
-    
-    auto& viewShadowNode =
-      static_cast<IosUtilitiesViewShadowNode&>(shadowNode);
-
-    react_native_assert(
-      dynamic_cast<YogaLayoutableShadowNode*>(&viewShadowNode)
-    );
-        
-    auto& layoutableShadowNode =
-      dynamic_cast<YogaLayoutableShadowNode&>(viewShadowNode);
-      
-    auto rawState = shadowNode.getState();
-
-    auto state = std::static_pointer_cast<
-       const IosUtilitiesViewShadowNode::ConcreteState
-     >(rawState);
-            
-    IosUtilitiesViewState stateData = state->getData();
-    Size newSize = stateData.frameSize;
-    
-    LayoutMetrics layoutMetrics = viewShadowNode.getLayoutMetrics();
-    Size oldSize = layoutMetrics.frame.size;
-    
-    // NOTE: `Size` impl. custom `!=` op for checking inequality
-    bool didChangeSize = newSize != oldSize;
-    bool isNewSizeValid = newSize.width != 0 && newSize.height != 0;
-
-    if (didChangeSize && isNewSizeValid) {
-      layoutableShadowNode.setSize(newSize);
-    };
-
-    ConcreteComponentDescriptor::adopt(shadowNode);
-  }
+public:
+  using RNIBaseViewComponentDescriptor::RNIBaseViewComponentDescriptor;
 };
 
-} // namespace react
-} // namespace facebook
+} // namespace facebook::react
---
+++ ios/Sources/RNIBaseView/RNIBaseViewComponentDescriptor.h
@@ -0,0 +1,65 @@
+//
+//  RNIBaseViewComponentDescriptor.cpp
+//  react-native-ios-utilities
+//
+//  Created by Dominic Go on 5/7/24.
+//
+
+#pragma once
+
+#include "RNIBaseViewState.h"
+
+#include <react/debug/react_native_assert.h>
+#include <react/renderer/core/ConcreteComponentDescriptor.h>
+#include <react/renderer/core/ConcreteState.h>
+#include <react/renderer/components/view/ConcreteViewShadowNode.h>
+
+
+namespace facebook::react {
+
+template <typename ShadowNodeT>
+class RNIBaseViewComponentDescriptor
+  : public ConcreteComponentDescriptor<ShadowNodeT> {
+  
+public:
+  using ConcreteComponentDescriptor<ShadowNodeT>::ConcreteComponentDescriptor;
+  
+  void adopt(ShadowNode& shadowNode) const override {
+    react_native_assert(
+      dynamic_cast<YogaLayoutableShadowNode*>(&shadowNode)
+    );
+        
+    auto& layoutableShadowNode =
+      dynamic_cast<YogaLayoutableShadowNode&>(shadowNode);
+      
+    auto rawState = shadowNode.getState();
+
+    auto state = std::dynamic_pointer_cast<
+      const ConcreteState<RNIBaseViewState>
+    >(rawState);
+     
+    if(state == nullptr){
+      ConcreteComponentDescriptor<ShadowNodeT>::adopt(shadowNode);
+      return;
+    };
+    
+    RNIBaseViewState stateData = state->getData();
+    Size newSize = stateData.frameSize;
+    
+    LayoutMetrics layoutMetrics = layoutableShadowNode.getLayoutMetrics();
+    Size oldSize = layoutMetrics.frame.size;
+    
+    // NOTE: `Size` impl. custom `!=` op overload for checking inequality
+    bool didChangeSize = newSize != oldSize;
+    bool isNewSizeValid = newSize.width != 0 && newSize.height != 0;
+
+    if (didChangeSize && isNewSizeValid) {
+      layoutableShadowNode.setSize(newSize);
+    };
+
+    ConcreteComponentDescriptor<ShadowNodeT>::adopt(shadowNode);
+  }
+};
+
+} // facebook::react
+
