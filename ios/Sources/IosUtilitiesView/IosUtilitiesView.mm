e44dfba57a4798432ccd26906d6296ae4e764e74
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2024-05-13 02:13:40 +0800

    💫 Update: `IosUtilitiesView`

--- ios/Sources/IosUtilitiesView/IosUtilitiesView.mm
+++ ios/Sources/IosUtilitiesView/IosUtilitiesView.mm
@@ -4,8 +4,14 @@
 
 #import "Utils.h"
 #import "react-native-ios-utilities/Swift.h"
+#import "react-native-ios-utilities/UIApplication+RNIHelpers.h"
+#import "react-native-ios-utilities/UIView+RNIFabricHelpers.h"
 
 #import <react/renderer/components/RNIosUtilitiesViewSpec/RCTComponentViewHelpers.h>
+#import <react/renderer/components/RNIosUtilitiesViewSpec/Props.h>
+#import <react/renderer/uimanager/UIManager.h>
+
+#import "RCTAppDelegate.h"
 #import "RCTFabricComponentsPlugins.h"
 
 
@@ -37,15 +43,24 @@ using namespace facebook::react;
 {
   const auto &oldViewProps = *std::static_pointer_cast<IosUtilitiesViewProps const>(_props);
   const auto &newViewProps = *std::static_pointer_cast<IosUtilitiesViewProps const>(props);
-
-  if (oldViewProps.color != newViewProps.color) {
-    const char *colorStringRaw = newViewProps.color.c_str();
-    NSString *colorString = [NSString stringWithUTF8String:colorStringRaw];
-    UIColor *color = [Utils hexStringToColor:colorString];
-    [self.contentView setBackgroundColor: color];
-  }
-
+  
   [super updateProps:props oldProps:oldProps];
+}
+
+- (void)didMoveToWindow
+{
+  if(self.window == nil){
+    return;
+  };
+  
+  facebook::react::UIManager *uiManager =
+    [[UIApplication sharedApplication] reactUIManager];
+    
+  RCTComponentViewRegistry *componentViewRegistry =
+    [[UIApplication sharedApplication] reactComponentViewRegistry];
+    
+
+  //[componentViewRegistry ]
 }
 
 // MARK: - RNIBaseView
