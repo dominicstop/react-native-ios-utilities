9f39d51b075f517e20d098d831462780c8aefb7a
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2024-05-13 18:17:24 +0800

    ⭐️ Impl: `RNIContentView`

---
+++ ios/Sources/RNIContentView/RNIContentView.swift
@@ -0,0 +1,12 @@
+//
+//  RNIContentView.swift
+//  react-native-ios-utilities
+//
+//  Created by Dominic Go on 5/13/24.
+//
+
+import Foundation
+import DGSwiftUtilities
+
+typealias RNIContentView =
+  RNIContentViewDelegate & StringKeyPathMapping;
