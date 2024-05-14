86d7fa68e48fba28b1903d032a12a11b8019fe05
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2024-05-15 07:35:43 +0800

    ⭐️ Impl: `RNIContentViewParent`

--- ios/Sources/RNIContentView/RNIContentViewParentDelegate.swift
+++ ios/Sources/RNIContentView/RNIContentViewParentDelegate.swift
@@ -6,9 +6,17 @@
 //
 
 import UIKit
+import React
+
+
+#if RCT_NEW_ARCH_ENABLED
+public typealias RNIContentViewParent = UIView
+#else
+public typealias RNIContentViewParent = RCTView;
+#endif
 
 @objc
-public protocol RNIContentViewParentDelegate where Self: UIView  {
+public protocol RNIContentViewParentDelegate where Self: RNIContentViewParent {
 
   var cachedLayoutMetrics: RNILayoutMetrics { get };
   
