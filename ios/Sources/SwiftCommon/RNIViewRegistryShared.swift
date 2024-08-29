87ec6f43cb5d0194cc0c4a3612af0315f4ada777
Dominic Go <dominic@dominicgo.dev>
2024-08-29 20:19:08 +0800

    ⭐️ Impl: `RNIViewRegistryShared`

---
+++ ios/Sources/SwiftCommon/RNIViewRegistryShared.swift
@@ -0,0 +1,10 @@
+//
+//  RNIViewRegistryShared.swift
+//  react-native-ios-utilities
+//
+//  Created by Dominic Go on 8/29/24.
+//
+
+import Foundation
+
+public let RNIViewRegistryShared = RNIViewRegistry.shared();
