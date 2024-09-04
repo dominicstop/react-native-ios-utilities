36f7fd53e4544728093835bdca111c6a6ed3d50f
Dominic Go <dominic@dominicgo.dev>
2024-09-04 23:52:42 +0800

    ⭐️ Impl: Types - `VerticalAlignmentPosition`

---
+++ src/types/DGSwiftUtilities/VerticalAlignmentPosition.ts
@@ -0,0 +1,7 @@
+
+export type VerticalAlignmentPosition =
+  | 'stretch'
+  | 'stretchTarget'
+  | 'targetTop'
+  | 'targetBottom'
+  | 'targetCenter';
--- src/types/DGSwiftUtilities/index.ts
+++ src/types/DGSwiftUtilities/index.ts
@@ -2,5 +2,6 @@
 export * from './Angle';
 export * from './AnimationConfig';
 export * from './HorizontalAlignmentPosition';
+export * from './VerticalAlignmentPosition';
 export * from './Transform3D';
 export * from './VerticalAnchorPositionMode';
