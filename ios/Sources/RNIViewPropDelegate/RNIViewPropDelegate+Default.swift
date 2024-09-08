acdc7aaa5b32c315860b67a8c1c9dca7ebe1b86e
Dominic Go <dominic@dominicgo.dev>
2024-09-09 07:57:22 +0800

    💫 Update: `RNIViewPropDelegate+Default`

--- ios/Sources/RNIViewPropDelegate/RNIViewPropDelegate+Default.swift
+++ ios/Sources/RNIViewPropDelegate/RNIViewPropDelegate+Default.swift
@@ -6,18 +6,19 @@
 //
 
 import Foundation
-
+import DGSwiftUtilities
 
 @objc
 fileprivate extension UIView {
   
   #if RCT_NEW_ARCH_ENABLED
   // @objc(_RNIViewPropDelegate_notifyOnRequestToSetPropsWithSender:props:)
+  @objc
   func _notifyOnRequestToSetProps(
     sender: RNIContentViewParentDelegate,
     props: NSDictionary
   ) {
-    guard let _self = self as? (any RNIContentView) else { return };
+    guard let _self = self as? (any RNIViewPropDelegate & StringKeyPathMapping) else { return };
     _self.setValues(withDict: props);
   };
   #endif
