110c8894153cdcc13b452f24b70d9a44afa909d2
Dominic Go <dominic@dominicgo.dev>
2024-09-10 09:38:07 +0800

    ⭐️ Impl: Types - `NativeComponentBasePropsInternal`

--- src/types/ReactNativeUtilityTypes.ts
+++ src/types/ReactNativeUtilityTypes.ts
@@ -1,6 +1,16 @@
 import type { ViewProps } from "react-native";
 import type { SharedViewEvents } from "./SharedViewEvents";
+import type { SharedViewEventsInternal } from "./SharedViewEventsInternal";
+import type { Merge } from "./UtilityTypes";
 
 
 export type NativeComponentBaseProps<T extends SharedViewEvents> =
   Omit<T, keyof (ViewProps & SharedViewEvents)>;
+
+export type NativeComponentBasePropsInternal<
+  T extends Merge<SharedViewEvents, SharedViewEventsInternal>
+> = Omit<T, keyof (
+    ViewProps
+  & SharedViewEvents
+  & SharedViewEventsInternal
+)>;
