2d462eea3b523cee65df224ad1e26f942f4ac81f
Dominic Go <dominic@dominicgo.dev>
2024-09-10 09:36:37 +0800

    ⭐️ Impl: Types - `Merge`

--- src/types/UtilityTypes.ts
+++ src/types/UtilityTypes.ts
@@ -14,3 +14,14 @@ export type UniformKeyAndValueFromObject<T extends Record<string, unknown>> = { 
 export type RemapObject<T, U extends {[key in keyof T]: unknown}> = {
   [TKey in keyof T]: U[TKey];
 };
+
+export type Merge<A, B> = {
+  [K in keyof A | keyof B]: 
+    K extends keyof A & keyof B
+    ? A[K] | B[K]
+    : K extends keyof B
+    ? B[K]
+    : K extends keyof A
+    ? A[K]
+    : never;
+};
