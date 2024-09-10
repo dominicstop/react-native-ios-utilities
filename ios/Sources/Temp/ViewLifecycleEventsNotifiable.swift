4cef9ff010bbb15959a7e86df6f02c48cc4d54a2
Dominic Go <dominic@dominicgo.dev>
2024-09-11 04:10:53 +0800

    ⭐️ Impl: `ViewLifecycleEventsNotifiable`

---
+++ ios/Sources/Temp/ViewLifecycleEventsNotifiable.swift
@@ -0,0 +1,81 @@
+//
+//  ViewLifecycleEventsNotifiable.swift
+//  react-native-ios-utilities
+//
+//  Created by Dominic Go on 9/11/24.
+//
+
+import Foundation
+
+public protocol ViewLifecycleEventsNotifiable: AnyObject {
+  
+  func notifyOnViewWillMoveToWindow(
+    sender: UIView,
+    newWindow: UIWindow?
+  );
+  
+  func notifyOnViewDidMoveToWindow(
+    sender: UIView
+  );
+  
+  func notifyOnViewWillMoveToSuperview(
+    sender: UIView,
+    newSuperview: UIView?
+  );
+  
+  func notifyOnViewDidMoveToSuperview(
+    sender: UIView
+  );
+  
+  func notifyOnViewLayoutSubviews(
+    sender: UIView
+  );
+  
+  func notifyOnViewRemovedFromSuperview(
+    sender: UIView
+  );
+};
+
+// MARK: - ViewLifecycleEventsNotifiable+Default
+// ---------------------------------------------
+
+public extension ViewLifecycleEventsNotifiable {
+  
+  func notifyOnViewWillMoveToWindow(
+    sender: UIView,
+    newWindow: UIWindow?
+  ) {
+    // no-op
+  };
+  
+  func notifyOnViewDidMoveToWindow(
+    sender: UIView
+  ) {
+    // no-op
+  };
+  
+  func notifyOnViewWillMoveToSuperview(
+    sender: UIView,
+    newSuperview: UIView?
+  ) {
+    // no-op
+  };
+  
+  func notifyOnViewDidMoveToSuperview(
+    sender: UIView
+  ) {
+    // no-op
+  };
+  
+  func notifyOnViewLayoutSubviews(
+    sender: UIView
+  ) {
+    // no-op
+  };
+  
+  func notifyOnViewRemovedFromSuperview(
+    sender: UIView
+  ) {
+    // no-op
+  };
+};
