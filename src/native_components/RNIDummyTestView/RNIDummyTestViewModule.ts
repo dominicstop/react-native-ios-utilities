ee65e52a74c389cb6e70026aeb48ed2385718b4a
Dominic Go <dominic@dominicgo.dev>
2024-09-19 12:40:30 +0800

    💫 Update: Ex - Fix `RNIDummyTestViewModule`

--- src/native_components/RNIDummyTestView/RNIDummyTestViewModule.ts
+++ src/native_components/RNIDummyTestView/RNIDummyTestViewModule.ts
@@ -2,62 +2,65 @@ import { RNIUtilitiesModule, type SharedNativeValueMap } from '../../native_modu
 
 const MODULE_NAME = "RNIDummyTestViewModule";
 
-export async function somePromiseCommandThatWillAlwaysResolve(
-  someString: string,
-  someNumber: number,
-  someBool: boolean,
-  someObject: Record<string, unknown>,
-  someArray: Array<unknown>,
-  someStringOptional: string | undefined = undefined
-): Promise<Record<string, unknown>>{
+export class RNIDummyTestViewModule {
 
-  return await RNIUtilitiesModule.moduleCommandRequest(
-    MODULE_NAME,  
-    "somePromiseCommandThatWillAlwaysResolve", 
-    {
-      someString,
-      someNumber,
-      someBool,
-      someObject,
-      someArray,
-      someStringOptional
-    }
-  );
-};
+  static async somePromiseCommandThatWillAlwaysResolve(
+    someString: string,
+    someNumber: number,
+    someBool: boolean,
+    someObject: Record<string, unknown>,
+    someArray: Array<unknown>,
+    someStringOptional: string | undefined = undefined
+  ): Promise<Record<string, unknown>>{
 
-export async function somePromiseCommandThatWillAlwaysReject(){
-  await RNIUtilitiesModule.moduleCommandRequest(
-    MODULE_NAME,  
-    "somePromiseCommandThatWillAlwaysReject",
-    {}
-  );
-};
+    return await RNIUtilitiesModule.moduleCommandRequest(
+      MODULE_NAME,  
+      "somePromiseCommandThatWillAlwaysResolve", 
+      {
+        someString,
+        someNumber,
+        someBool,
+        someObject,
+        someArray,
+        someStringOptional
+      }
+    );
+  };
 
-export function overwriteModuleSharedValues(newValues: SharedNativeValueMap) {
-  RNIUtilitiesModule.overwriteModuleSharedValues(
-    MODULE_NAME,
-    newValues
-  );
-};
+  static async somePromiseCommandThatWillAlwaysReject(){
+    await RNIUtilitiesModule.moduleCommandRequest(
+      MODULE_NAME,  
+      "somePromiseCommandThatWillAlwaysReject",
+      {}
+    );
+  };
 
-export function getAllModuleSharedValues(): {
-  someNumber: number | undefined
-} {
-  //@ts-ignore
-  return RNIUtilitiesModule.getAllModuleSharedValues(MODULE_NAME);
-}
+  static overwriteModuleSharedValues(newValues: SharedNativeValueMap) {
+    RNIUtilitiesModule.overwriteModuleSharedValues(
+      MODULE_NAME,
+      newValues
+    );
+  };
 
-export function getSharedValueSomeNumber(): number | undefined {
-  return RNIUtilitiesModule.getModuleSharedValue(
-    MODULE_NAME,  
-    "someNumber"
-  );
-};
+  static getAllModuleSharedValues(): {
+    someNumber: number | undefined
+  } {
+    //@ts-ignore
+    return RNIUtilitiesModule.getAllModuleSharedValues(MODULE_NAME);
+  }
 
-export function setSharedValueSomeNumber(newValue: number | undefined){
-  RNIUtilitiesModule.setModuleSharedValue(
-    MODULE_NAME,  
-    "someNumber",
-    newValue
-  );
+  static getSharedValueSomeNumber(): number | undefined {
+    return RNIUtilitiesModule.getModuleSharedValue(
+      MODULE_NAME,  
+      "someNumber"
+    );
+  };
+
+  static setSharedValueSomeNumber(newValue: number | undefined){
+    RNIUtilitiesModule.setModuleSharedValue(
+      MODULE_NAME,  
+      "someNumber",
+      newValue
+    );
+  };
 };
