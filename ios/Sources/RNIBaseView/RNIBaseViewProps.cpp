df8c4e28ecf17039da99f3c2bdaaa33f371d9539
Dominic Go <dominic@dominicgo.dev>
2024-10-25 20:12:43 +0800

    💫 Update: `RNIBaseViewProps` Constructor

--- ios/Sources/RNIBaseView/RNIBaseViewProps.cpp
+++ ios/Sources/RNIBaseView/RNIBaseViewProps.cpp
@@ -21,14 +21,31 @@ RNIBaseViewProps::RNIBaseViewProps(
 ): ViewProps(context, sourceProps, rawProps) {
   
   std::unordered_map<std::string, folly::dynamic> propsMap = std::move(sourceProps.propsMap);
-
+  
+  #if REACT_NATIVE_TARGET_VERSION >= 76
+  /// PR-#6 by: SamuelScheit
+  /// Link: https://github.com/dominicstop/react-native-ios-utilities/pull/6
+  ///
+  /// * This is more performant
+  /// * `iterateOverValues` has been deprecated on RN 0.76+
+  ///
   const auto& dynamicProps = static_cast<folly::dynamic>(rawProps);
-
+  
   for (const auto& pair : dynamicProps.items()) {
     const auto& name = pair.first.getString();
     propsMap[name] = pair.second;
-  }
-  
+  };
+  #else
+  rawProps.iterateOverValues([&propsMap](
+    react::RawPropsPropNameHash hash,
+    const char *name,
+    const react::RawValue &value
+  ) {
+    std::string propName(name);
+    propsMap[propName] = (folly::dynamic)value;
+  });
+  #endif
+
   this->propsMap = propsMap;
 }
 #endif
