8e16dbc54ba841981001ffe8989b79d56195c04a
Dominic Go <dominic@dominicgo.dev>
2024-10-30 14:59:32 +0800

    ⚙️ Chore: Ex - Run `pod-install`

--- example/ios/reactnativeiosutilitiesexample/AppDelegate.mm
+++ example/ios/reactnativeiosutilitiesexample/AppDelegate.mm
@@ -18,8 +18,14 @@
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
 {
+  return [self bundleURL];
+}
+
+- (NSURL *)bundleURL
+{
 #if DEBUG
-  return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@".expo/.virtual-metro-entry"];
+  return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index"];
+  // return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@".expo/.virtual-metro-entry"];
 #else
   return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
 #endif
