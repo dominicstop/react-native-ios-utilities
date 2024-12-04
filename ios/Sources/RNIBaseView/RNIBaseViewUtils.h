c2434a9d64497f9d70ace7e0db9f67855f841cda
Dominic Go <dominicstop@gmail.com>
2024-12-05 07:22:51 +0800

    Revert "fix: dynamic framework compatibility"
    This reverts commit adcd6f38a9e8719459f729bef14603db6807f15c.

--- ios/Sources/RNIBaseView/RNIBaseViewUtils.h
+++ ios/Sources/RNIBaseView/RNIBaseViewUtils.h
@@ -5,16 +5,8 @@
 //  Created by Dominic Go on 5/14/24.
 //
 
-#if RCT_NEW_ARCH_ENABLED
-#define RNI_EXPORT_VIEW_PROPERTY(propName, type) \
-  RCT_CUSTOM_VIEW_PROPERTY(propName, id, RNIBaseView){}
-
-#define RNI_EXPORT_VIEW_EVENT(eventName, type) \
-  RCT_CUSTOM_VIEW_PROPERTY(eventName, id, RNIBaseView){}
-#else  
 #define RNI_EXPORT_VIEW_PROPERTY(propName, type) \
   RCT_REMAP_VIEW_PROPERTY(propName, reactPropHandler.propName, type)
-
+  
 #define RNI_EXPORT_VIEW_EVENT(eventName, type) \
   RCT_REMAP_VIEW_PROPERTY(eventName, reactEventHandler.eventName, type)
-#endif
