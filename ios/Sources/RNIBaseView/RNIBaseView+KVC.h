1b35626fdb742e7eae8db8b67c2e1f2abbbf82ac
Dominic Go <dominic@dominicgo.dev>
2024-12-11 12:20:16 +0800

    💄 Gloss: Update PR-Related Comment

--- ios/Sources/RNIBaseView/RNIBaseView+KVC.h
+++ ios/Sources/RNIBaseView/RNIBaseView+KVC.h
@@ -6,26 +6,17 @@ NS_ASSUME_NONNULL_BEGIN
 /// PR-#11 by: coolsoftwaretyler
 /// Link: https://github.com/dominicstop/react-native-ios-utilities/pull/11
 ///
-/// * it seems that these properties are being invoked by fabric:
-///   `reactEventHandler`, and `reactPropHandler` (these are not currently
-///    impl. in `RNIBaseView`, and will cause a crash).
+/// * When using new arch + dynamic linkage, the fabric views don't get
+///   initialized
 ///
-/// * these properties return a map of the props/events names and their
-///   corresponding handler.
+/// * The specs for the fabric views are defined in:
+///   `RCTThirdPartyFabricComponentsProvider`
 ///
-/// * the PR intercepts the calls for `reactEventHandler` and `reactPropHandler`
-///   and returns dummy handlers for: `onDidSetViewID`, `onViewWillRecycle`,
-///   `onRawNativeEvent`.
+/// * When dynamic linkage is enabled, none of the specs defined are exported
+///   due to `RCT_DYNAMIC_FRAMEWORKS` being set to false.
 ///
-/// * currently, the props for fabric are only intercepted using the
-///   `updateProps` lifecycle method; perhaps this is not enough by itself.
-///
-/// * the commit states: "fix: dynamic framework compatibility"; this could
-///   be the reason why the breakpoints aren't being triggered.
-///
-/// * TODO: create a permanent fix
-/// * read the prop map + event list from swift, and vend the
-///   "props/events handlers map" for fabric.
+/// * Related issue:
+///   https://github.com/software-mansion/react-native-screens/issues/2228
 ///
 @interface RNIBaseView (KVC)
 
