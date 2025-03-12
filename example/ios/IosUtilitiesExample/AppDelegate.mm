0cd2a2073620d2daa9534977542f294fe1a7295c
Dominic Go <dominic@dominicgo.dev>
2025-03-12 13:34:00 +0800

    💫 Update: Ex - `AppDelegate`

--- example/ios/IosUtilitiesExample/AppDelegate.mm
+++ example/ios/IosUtilitiesExample/AppDelegate.mm
@@ -1,22 +1,20 @@
 #import "AppDelegate.h"
 
 #import <React/RCTBundleURLProvider.h>
+#import <ReactAppDependencyProvider/RCTAppDependencyProvider.h>
+
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
   self.moduleName = @"IosUtilitiesExample";
+  self.dependencyProvider = [RCTAppDependencyProvider new];
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
-
+  
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
-}
-
-- (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
-{
-  return [self bundleURL];
 }
 
 - (NSURL *)bundleURL
@@ -27,5 +25,4 @@
   return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
 #endif
 }
-
 @end
