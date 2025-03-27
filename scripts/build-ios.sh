19d21862af4d2dbebdcd9330acd2f48fc10d2841
Dominic Go <dominic@dominicgo.dev>
2025-03-27 14:50:52 +0800

    ⚙️ Chore: Update `build-ios.sh` Script

--- scripts/build-ios.sh
+++ scripts/build-ios.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-BUILD_CONFIG="${1:-DEBUG}"
+ARG_BUILD_CONFIG="${1:-DEBUG}"
 
 cd ./example/ios
 
@@ -10,16 +10,29 @@ BUILD_INFO=$(
     -showBuildSettings -list -json | tr -d ' ' | tr -d '\n'
 )
 
+BUILD_CONFIG=""
+if [ "$ARG_BUILD_CONFIG" == "DEBUG" ]; then
+  BUILD_CONFIG="Debug"
+elif [ "$ARG_BUILD_CONFIG" == "RELEASE" ]; then
+  BUILD_CONFIG="Release"
+else
+  echo "Error: Unknown build configuration '$ARG_BUILD_CONFIG'. Please specify 'DEBUG' or 'RELEASE'."
+  exit 1
+fi
+
 SCHEME_NAME=$(
-  node -pe 'JSON.parse(process.argv[1]).project.schemes[0]' $BUILD_INFO
-) 
+  node -pe 'JSON.parse(process.argv[1]).project.schemes[0]' "$BUILD_INFO"
+)
+
 echo "Starting build..."
-echo "Configuration: $BUILD_CONFIG"
-echo "Scheme: $SCHEME_NAME"
+echo " - Configuration: $ARG_BUILD_CONFIG"
+echo " - Scheme: $SCHEME_NAME"
 
 xcodebuild \
   -workspace *.xcworkspace \
-  -configuration Release \
-  -scheme $SCHEME_NAME \
+  -configuration "$BUILD_CONFIG" \
+  -scheme "$SCHEME_NAME" \
   -destination 'generic/platform=iOS' \
+  CODE_SIGNING_REQUIRED=NO \
+  CODE_SIGNING_ALLOWED=NO \
   clean build
