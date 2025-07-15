80c69685d2efd877698ee7c16f77adeb2313a1ac
Dominic Go <dominic@dominicgo.dev>
2025-07-15 10:38:08 +0800

    ⚙️ Chore: Update `scripts/echo-all-example-ios-paths.sh`

--- scripts/echo-all-example-ios-paths.sh
+++ scripts/echo-all-example-ios-paths.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+LIB_ROOT_DIR_NAME="react-native-ios-utilities"
+
+# CD to `LIB_ROOT_DIR_NAME`
+goToLibRootIfNeeded() {
+  CURRENT_DIR="$(pwd)"
+  while [ "$(basename "$CURRENT_DIR")" != "$LIB_ROOT_DIR_NAME" ]; do
+    if [ "$CURRENT_DIR" = "/" ]; then
+      return 1
+    fi
+    CURRENT_DIR="$(dirname "$CURRENT_DIR")"
+  done
+
+  cd "$CURRENT_DIR" || return 1
+  return 0
+}
+
+goToLibRootIfNeeded
+
 IOS_DIR_ITEMS=()
 
 # append all in: `examples/**/ios`
@@ -7,7 +25,7 @@ if [ -d "examples" ]; then
   for dir in examples/*/; do
 
     # skip if not a directory...
-    [ -d "$dir" ] || continue 
+    [ -d "$dir" ] || continue
 
     if [ -d "${dir}ios" ]; then
       IOS_DIR_ITEMS+=("${dir}ios")
@@ -15,7 +33,7 @@ if [ -d "examples" ]; then
   done
 fi
 
-IOS_DIR_ITEMS+=("example/ios" "ios")
+IOS_DIR_ITEMS+=("example/ios")
 
 # Filter only valid paths into IOS_DIR_PATHS
 IOS_DIR_PATHS=()
