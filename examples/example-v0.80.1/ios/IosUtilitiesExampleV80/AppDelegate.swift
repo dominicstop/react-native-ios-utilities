9f2556b570b4927503c4654d6bb7b850318a080d
Dominic Go <dominic@dominicgo.dev>
2025-07-15 20:27:36 +0800

    ⚙️ Chore: Ex - Regenerate iOS Project for `example-v0.80.1`

--- examples/example-v0.80.1/ios/IosUtilitiesExampleV80/AppDelegate.swift
+++ examples/example-v0.80.1/ios/IosUtilitiesExampleV80/AppDelegate.swift
@@ -24,7 +24,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     window = UIWindow(frame: UIScreen.main.bounds)
 
     factory.startReactNative(
-      withModuleName: "IosUtilitiesExample",
+      withModuleName: "IosUtilitiesExampleV80",
       in: window,
       launchOptions: launchOptions
     )
