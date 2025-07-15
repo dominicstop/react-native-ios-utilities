e9b38127061625c1986b7c87aa584cc6e0adc950
Dominic Go <dominic@dominicgo.dev>
2025-07-15 13:26:01 +0800

    ⚙️ Chore: Update `scripts/set-node-env.sh`

--- scripts/set-node-env.sh
+++ scripts/set-node-env.sh
@@ -41,14 +41,28 @@ NVM_DIR="$HOME/.nvm"
 # try load NVM
 if [ $IS_NODE_BINARY_PATH_BAD == 1 ]; then
   echo "Warning: NODE_BINARY path malformed: $NODE_BINARY_PATH"
-  echo "Checking if NVM installed..."
+  echo "Task: Checking if NVM installed..."
 
   if [ -s "$NVM_DIR/nvm.sh" ]; then
     # shellcheck source=/dev/null
     \. "$NVM_DIR/nvm.sh"
     IS_NVM_INSTALLED=1
 
-    echo "NVM loaded successfully."
+    echo "Task: NVM loaded successfully."
+    echo "Task: NVM - Checking if stable is installed..."
+
+    # Use stable version, install if not present
+    if ! nvm ls stable | grep -q '->'; then
+      echo "Task: NVM - Stable Node.js version not found. Installing..."
+      nvm install stable
+
+    else
+      echo "Task: NVM - Stable Node.js version already installed. Skipping..."
+    fi
+
+    echo "Task: NVM - Setting node version to stable..."
+    nvm use stable
+
     NODE_BINARY_PATH=$(command -v node)
     echo "Using NVM for NODE_BINARY_PATH: $NODE_BINARY_PATH"
   fi
