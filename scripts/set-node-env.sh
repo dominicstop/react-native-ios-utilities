714016d3a5cd53387906dd08ebef676cf2cd5c7c
Dominic Go <dominic@dominicgo.dev>
2025-07-15 08:59:50 +0800

    ⚙️ Chore: Fix Script `set-node-env.sh`

--- scripts/set-node-env.sh
+++ scripts/set-node-env.sh
@@ -5,7 +5,24 @@
 # * This is used as a fallback
 FALLBACK="export NODE_BINARY=\$(command -v node)"
 
-cd example/ios
+# `cd ./example/ios`
+IOS_DIR_ITEMS=(
+  "ios"
+  "examples/example-core/ios"
+)
+
+# set directory if needed
+if [ -d "ios" ]; then
+  for directory in "${IOS_DIR_ITEMS[@]}"; do
+    if [ -d "$directory" ]; then
+      cd "$directory"
+      echo "Current DIR: $(pwd)"
+      echo "Changed directory to: $directory"
+      break
+    fi
+  done
+fi
+
 NODE_BINARY_PATH=$(command -v node)
 
 IS_NODE_BINARY_PATH_BAD=0
@@ -18,11 +35,34 @@ fi
 
 echo "Writting: .xcode.env and .xcode.env.local"
 
+IS_NVM_INSTALLED=0
+NVM_DIR="$HOME/.nvm"
+
+# try load NVM
 if [ $IS_NODE_BINARY_PATH_BAD == 1 ]; then
   echo "Warning: NODE_BINARY path malformed: $NODE_BINARY_PATH"
+  echo "Checking if NVM installed..."
+
+  if [ -s "$NVM_DIR/nvm.sh" ]; then
+    # shellcheck source=/dev/null
+    \. "$NVM_DIR/nvm.sh"
+    IS_NVM_INSTALLED=1
+
+    echo "NVM loaded successfully."
+    NODE_BINARY_PATH=$(command -v node)
+    echo "Using NVM for NODE_BINARY_PATH: $NODE_BINARY_PATH"
+  fi
+
+else
+  echo "Status: NODE_BINARY_PATH not malformed"
+fi
+
+if [[ $IS_NODE_BINARY_PATH_BAD == 1 && $IS_NVM_INSTALLED == 0 ]]; then
+  echo "Warning: NVM not found..."
   echo "Using fallback: $FALLBACK"
   echo "$FALLBACK" > .xcode.env
   echo "$FALLBACK" > .xcode.env.local
+
 else
   FILE_CONTENTS="export NODE_BINARY=$NODE_BINARY_PATH"
 
